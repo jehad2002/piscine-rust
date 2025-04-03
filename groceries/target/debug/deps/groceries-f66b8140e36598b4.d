df7ef7724c5576464d303edc082adeb8492ae85e
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2025-04-03 15:37:54 +0200

    rust

---
+++ groceries/target/debug/deps/groceries-f66b8140e36598b4.d
@@ -0,0 +1,5 @@
+/Users/adam-06-05/Desktop/piscine-rust/groceries/target/debug/deps/groceries-f66b8140e36598b4: src/main.rs
+
+/Users/adam-06-05/Desktop/piscine-rust/groceries/target/debug/deps/groceries-f66b8140e36598b4.d: src/main.rs
+
+src/main.rs:
---
+++ groceries/target/debug/groceries.d
@@ -0,0 +1,1 @@
+/Users/adam-06-05/Desktop/piscine-rust/groceries/target/debug/groceries: /Users/adam-06-05/Desktop/piscine-rust/groceries/src/lib.rs /Users/adam-06-05/Desktop/piscine-rust/groceries/src/main.rs
