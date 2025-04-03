3cf11ddde1288ae7c14b537ce26cc6f7c0a28a31
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2025-04-03 15:06:16 +0200

    rust

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -1,27 +1,24 @@
 use std::io;
 
 fn main() {
-    let riddle = "I am the beginning of the end, and the end of time and space. \
-                  I am essential to creation, and I surround every place. What am I?";
+    let riddle = "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
     let correct_answer = "The letter e";
-    
-    let mut trials = 0; 
+    let mut trials = 0;
 
     loop {
-        println!("{}", riddle); 
-
+        println!("{}", riddle);
+        
         let mut answer = String::new();
         io::stdin().read_line(&mut answer).expect("Failed to read line");
 
         let answer = answer.trim();
-
-        trials += 1; 
+        trials += 1;
 
         if answer == correct_answer {
-            println!("Number of trials: {}", trials); 
-            break; 
+            println!("Number of trials: {}", trials);
+            break;
         } else {
-            println!("Incorrect answer. Try again!"); 
+            println!("Incorrect answer. Try again!");
         }
     }
 }
