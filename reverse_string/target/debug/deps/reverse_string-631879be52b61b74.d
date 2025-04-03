7ed5c84677899cf4676c38755c228555f31ff0ad
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2025-04-03 15:57:43 +0200

    rust

--- reverse_string/target/debug/deps/reverse_string-631879be52b61b74.d
+++
@@ -1,5 +0,0 @@
-/Users/adam-06-05/Desktop/piscine-rust/reverse_string/target/debug/deps/reverse_string-631879be52b61b74: src/main.rs
-
-/Users/adam-06-05/Desktop/piscine-rust/reverse_string/target/debug/deps/reverse_string-631879be52b61b74.d: src/main.rs
-
-src/main.rs:
--- reverse_string/target/debug/libreverse_string.d
+++
@@ -1,1 +0,0 @@
-/Users/adam-06-05/Desktop/piscine-rust/reverse_string/target/debug/libreverse_string.rlib: /Users/adam-06-05/Desktop/piscine-rust/reverse_string/src/lib.rs
