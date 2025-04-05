22684f10344d52f5d3535ef3ff16b51526828100
unknown <jehadalami11@gmail.com>
2025-04-05 23:42:25 +0300

    rust

--- ownership/src/lib.rs
+++ ownership/src/lib.rs
@@ -1,9 +1,13 @@
 pub fn first_subword(mut s: String) -> String {
+    // البحث عن أول حرف كبير أو underscore بعد أول حرف في السلسلة
 
     let index = match s.chars().skip(1).position(|c| c.is_uppercase() || c == '_') {
-        Some(idx) => idx+1,
-        None => return s, 
+
+        Some(idx) => idx+1, // إضافة 1 لتخطي الحرف الأول
+        None => return s,  // إذا لم يتم العثور على حرف كبير أو underscore، نعيد السلسلة الأصلية
     };
+        // تقصير السلسلة إلى الموضع المحدد
+
     s.truncate(index);
     s
 }
