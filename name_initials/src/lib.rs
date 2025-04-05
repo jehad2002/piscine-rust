115c7a81e2b191dc8f82f8e6afc7d22767852928
unknown <jehadalami11@gmail.com>
2025-04-05 21:12:17 +0300

    rust

--- name_initials/src/lib.rs
+++ name_initials/src/lib.rs
@@ -1,29 +1,9 @@
-// pub fn initials(names: Vec<&str>) -> Vec<String> {
-//     names.iter().map(|name| {
-//         name
-//             .split_whitespace()
-//             .map(|word| format!("{}.", word.chars().next().unwrap()))
-//             .collect::<Vec<_>>()
-//             .join(" ")
-//     }).collect()
-// }
-
-//==============================================
-
 pub fn initials(names: Vec<&str>) -> Vec<String> {
-    let mut result = Vec::new();
-
-    for name in names {
-        let mut initials = Vec::new();
-
-        for word in name.split_whitespace() {
-            if let Some(first_char) = word.chars().next() {
-                initials.push(format!("{}.", first_char));
-            }
-        }
-
-        result.push(initials.join(" "));
-    }
-
-    result
+    names.iter().map(|name| {
+        name
+            .split_whitespace()
+            .map(|word| format!("{}.", word.chars().next().unwrap()))
+            .collect::<Vec<_>>()
+            .join(" ")
+    }).collect()
 }
