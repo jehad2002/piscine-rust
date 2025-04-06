c57e2d22446294e2b80f5353dcdc0ee1847ce780
unknown <jehadalami11@gmail.com>
2025-04-06 19:04:23 +0300

    rust

--- card_deck/src/lib.rs
+++ card_deck/src/lib.rs
@@ -11,10 +11,45 @@ pub enum Suit {
 #[derive(Debug, PartialEq)]
 pub enum Rank {
     Ace,
-    King,
-    Queen,
-    Jack,
     Number(u8),
+    Jack,
+    Queen,
+    King,
+}
+
+impl Suit {
+    pub fn random() -> Suit {
+        let value = rand::thread_rng().gen_range(1..=4);
+        Self::translate(value)
+    }
+
+    pub fn translate(value: u8) -> Suit {
+        match value {
+            1 => Suit::Heart,
+            2 => Suit::Diamond,
+            3 => Suit::Spade,
+            4 => Suit::Club,
+            _ => panic!("Invalid suit value"),
+        }
+    }
+}
+
+impl Rank {
+    pub fn random() -> Rank {
+        let value = rand::thread_rng().gen_range(1..=13);
+        Self::translate(value)
+    }
+
+    pub fn translate(value: u8) -> Rank {
+        match value {
+            1 => Rank::Ace,
+            2..=10 => Rank::Number(value),
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            13 => Rank::King,
+            _ => panic!("Invalid rank value"),
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
@@ -23,34 +58,6 @@ pub struct Card {
     pub rank: Rank,
 }
 
-pub fn generate_suit() -> Suit {
-    let value = rand::thread_rng().gen_range(1..=4);
-    match value {
-        1 => Suit::Heart,
-        2 => Suit::Diamond,
-        3 => Suit::Spade,
-        _ => Suit::Club,
-    }
-}
-
-pub fn generate_rank() -> Rank {
-    let value = rand::thread_rng().gen_range(1..=13);
-    match value {
-        1 => Rank::Ace,
-        11 => Rank::Jack,
-        12 => Rank::Queen,
-        13 => Rank::King,
-        _ => Rank::Number(value),
-    }
-}
-
-pub fn generate_card() -> Card {
-    Card {
-        suit: generate_suit(),
-        rank: generate_rank(),
-    }
-}
-
 pub fn winner_card(card: &Card) -> bool {
-    matches!(card.rank, Rank::Ace) && matches!(card.suit, Suit::Spade)
+    matches!(card.suit, Suit::Spade) && matches!(card.rank, Rank::Ace)
 }
--- card_deck/src/main.rs
+++ card_deck/src/main.rs
@@ -1,19 +1,14 @@
 use card_deck::*;
 
 fn main() {
-    let your_card = generate_card();
-    println!("Your card: {:?}", your_card);
+    let your_card = Card {
+        rank: Rank::random(),
+        suit: Suit::random(),
+    };
+
+    println!("Your card is {:?}", your_card);
 
     if winner_card(&your_card) {
-        println!("Congratulations! You drew the winning card: Ace of Spades!");
-    } else {
-        println!("Not the winner card. Better luck next time!");
+        println!("You are the winner!");
     }
-
-    // Example test (can be removed or used for checking)
-    let test_card = Card {
-        suit: Suit::Spade,
-        rank: Rank::Ace,
-    };
-    assert_eq!(winner_card(&test_card), true);
 }
