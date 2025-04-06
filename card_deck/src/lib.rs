1e094f177669b358378cad3a4beb0ca9c2c0f680
unknown <jehadalami11@gmail.com>
2025-04-06 19:10:49 +0300

    rust

--- card_deck/src/lib.rs
+++ card_deck/src/lib.rs
@@ -11,16 +11,22 @@ pub enum Suit {
 #[derive(Debug, PartialEq)]
 pub enum Rank {
     Ace,
-    Number(u8),
-    Jack,
-    Queen,
     King,
+    Queen,
+    Jack,
+    Number(u8),
 }
 
 impl Suit {
     pub fn random() -> Suit {
-        let value = rand::thread_rng().gen_range(1..=4);
-        Self::translate(value)
+        let mut random = rand::thread_rng();
+        let i: i32 = random.gen_range(1..5);
+        match i {
+            1 => Suit::Club,
+            2 => Suit::Heart,
+            3 => Suit::Spade,
+            _ => Suit::Diamond,
+        }
     }
 
     pub fn translate(value: u8) -> Suit {
@@ -29,25 +35,32 @@ impl Suit {
             2 => Suit::Diamond,
             3 => Suit::Spade,
             4 => Suit::Club,
-            _ => panic!("Invalid suit value"),
+            _ => panic!("invalid suit!!"),
         }
     }
 }
 
 impl Rank {
     pub fn random() -> Rank {
-        let value = rand::thread_rng().gen_range(1..=13);
-        Self::translate(value)
+        let mut random = rand::thread_rng();
+        let i: i32 = random.gen_range(1..14);
+        match i {
+            1 => Rank::Ace,
+            11 => Rank::Jack,
+            12 => Rank::Queen,
+            13 => Rank::King,
+            i => Rank::Number(i as u8),
+        }
     }
 
     pub fn translate(value: u8) -> Rank {
         match value {
             1 => Rank::Ace,
-            2..=10 => Rank::Number(value),
             11 => Rank::Jack,
             12 => Rank::Queen,
             13 => Rank::King,
-            _ => panic!("Invalid rank value"),
+            value if value >= 2 && value < 11 => Rank::Number(value),
+            _ => panic!("invalid Rank!!"),
         }
     }
 }
@@ -59,5 +72,8 @@ pub struct Card {
 }
 
 pub fn winner_card(card: &Card) -> bool {
-    matches!(card.suit, Suit::Spade) && matches!(card.rank, Rank::Ace)
+    *card == Card {
+        suit: Suit::Spade,
+        rank: Rank::Ace,
+    }
 }
