58f2cb8bbefbfb4dc8887b703e22c313788dd0c0
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2025-04-08 16:00:34 +0200

    exam

--- reverse_it/src/lib.rs
+++ reverse_it/src/lib.rs
@@ -1,7 +1,27 @@
+// pub fn reverse_it(v: i32) -> String {
+//     let sign = if v < 0 { "-" } else { "" };
+//     let binding = v.to_string();
+//     let num = binding.trim_matches(|c| c == '-');
+//     let reverse = num.to_string().chars().rev().collect::<String>();
+//     format!("{}{}{}", sign, reverse, num)
+// }
+
+//=====================================
+
 pub fn reverse_it(v: i32) -> String {
-    let sign = if v < 0 { "-" } else { "" };
-    let binding = v.to_string();
-    let num = binding.trim_matches(|c| c == '-');
-    let reverse = num.to_string().chars().rev().collect::<String>();
-    format!("{}{}{}", sign, reverse, num)
+    let mut x = v;
+    let mut y :i32 = 0;
+    let mut sign = 1;
+    if v < 0 {
+        sign = -1;
+        x *= sign;
+    }
+    while x != 0 {
+        y *= 10;
+        y += x % 10;
+        x /= 10;
+        //println!("y = {y}, x = {x}");
+    }
+    y *= sign;
+    y.to_string()
 }
--- reverse_it/src/main.rs
+++ reverse_it/src/main.rs
@@ -1,15 +1,8 @@
-use reverse_it::*;
+use reverse_it::reverse_it;
 
 fn main() {
-    println!("{}", reverse_it(123456789));
+    println!("{}", reverse_it(123));
     println!("{}", reverse_it(-123));
 }
 
-#[test]
-fn reverse_it_test() {
-    assert_eq!("321123", &reverse_it(123));
-    assert_eq!("987654321123456789", &reverse_it(123456789));
-    assert_eq!("00", &reverse_it(0));
-    assert_eq!("-321123", &reverse_it(-123));
-    assert_eq!("11", &reverse_it(1));
-}
+
