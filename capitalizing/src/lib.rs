5be6f77bc95d4f39814434171fd47e7a17242203
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2025-04-08 10:46:05 +0200

    exam

--- capitalizing/src/lib.rs
+++ capitalizing/src/lib.rs
@@ -6,50 +6,21 @@ pub fn capitalize_first(input: &str) -> String {
 }
 
 pub fn title_case(input: &str) -> String {
-    input.split_whitespace().map(|word| {
-        capitalize_first(word)
-    }).collect::<Vec<String>>().join(" ")
-}
-
-pub fn change_case(input: &str) -> String {
-    input.chars().map(|c| {
-        if c.is_uppercase() {
-            c.to_lowercase().next().unwrap()
-        } else {
-            c.to_uppercase().next().unwrap()
-        }
-    }).collect()
-}
-
-//=====================================================
-
-pub fn capitalize_first(input: &str) -> String {
-    if let Some(first) = input.chars().next() {
-        let c = first.to_ascii_uppercase().to_string();
-        let r = input
-            .chars()
-            .skip(1)
-            .collect::<String>()
-            .to_ascii_lowercase();
-        return format!("{}{}", c, r);
-    }
-    String::new()
-}
-pub fn title_case(input: &str) -> String {
     input
         .split_whitespace()
-        .map(|s| capitalize_first(s))
+        .map(capitalize_first)
         .collect::<Vec<String>>()
         .join(" ")
 }
+
 pub fn change_case(input: &str) -> String {
     input
         .chars()
-        .map(|s| {
-            if s.is_ascii_uppercase() {
-                s.to_ascii_lowercase()
+        .map(|c| {
+            if c.is_uppercase() {
+                c.to_lowercase().next().unwrap()
             } else {
-                s.to_ascii_uppercase()
+                c.to_uppercase().next().unwrap()
             }
         })
         .collect()
