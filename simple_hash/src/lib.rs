cb8e6be5c2c61a17cc015b9a0662d098768052fc
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2025-04-08 12:59:06 +0200

    rust

--- simple_hash/src/lib.rs
+++ simple_hash/src/lib.rs
@@ -1,21 +1,15 @@
 use std::collections::HashMap;
 
 pub fn word_frequency_counter(words: Vec<&str>) -> HashMap<&str, usize> {
-    let mut result: HashMap<&str, usize> = HashMap::new();
+    let mut frequency_count = HashMap::new();
 
     for word in words {
-        *result.entry(word).or_insert(0) += 1;
+        *frequency_count.entry(word).or_insert(0) += 1;
     }
 
-    result
+    frequency_count
 }
 
 pub fn nb_distinct_words(frequency_count: &HashMap<&str, usize>) -> usize {
-    let mut count: usize = 0;
-    for &val in frequency_count.values() {
-        if val == 1 {
-            count += 1
-        }
-    }
-    count
+    frequency_count.len()
 }
