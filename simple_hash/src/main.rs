aa9c09af9ee55e41b8be480131a5b90f744d8f78
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2025-04-08 14:42:57 +0200

    rust

--- simple_hash/src/main.rs
+++ simple_hash/src/main.rs
@@ -3,10 +3,16 @@ use simple_hash::*;
 const SENTENCE: &str = "this is a very basic sentence with only a few repetitions. once again this is very basic but it should be enough for basic tests";
 
 
+
 fn main() {
     let words = vec!["this", "is", "a", "test", "test", "this"];
-    let frequency_count = word_frequency_counter(words); // تم تمرير `words` بدون المرجعية
+    
+    // تمرير `words` مباشرة بدون `&`
+    let frequency_count = word_frequency_counter(words); 
 
     println!("{:?}", frequency_count);
     println!("{}", nb_distinct_words(&frequency_count));
+
+    // اختبار مع مصفوفة فارغة
+    assert_eq!(solution(&[]), word_frequency_counter((&[]).to_vec()));
 }
