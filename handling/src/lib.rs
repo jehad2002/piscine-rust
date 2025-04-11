55ec7e3cb0aa97af2844a5276c65244c9cf49aa9
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2025-04-11 10:33:43 +0200

    rust

--- handling/src/lib.rs
+++ handling/src/lib.rs
@@ -1,17 +1,20 @@
 use std::fs::OpenOptions;
 use std::io::Write;
+use std::path::Path;
 
-pub fn open_or_create(file: &str, content: &str) {
+pub fn open_or_create<P: AsRef<Path>>(file: P, content: &str) {
     let mut f_h = match OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
-        .open(file) {
-            Ok(file) => file,
-            Err(err) => panic!("{}", err),
-        };
+        .open(file.as_ref())
+    {
+        Ok(file) => file,
+        Err(err) => panic!("{}", err),
+    };
 
     if let Err(err) = f_h.write_all(content.as_bytes()) {
         panic!("{}", err);
     }
 }
+//._.
