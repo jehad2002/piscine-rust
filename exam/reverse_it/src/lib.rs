3d5bfbc0bf029404d712e33d696808df175c119d
unknown <jehadalami11@gmail.com>
2025-04-14 21:28:49 +0300

    rust

--- exam/reverse_it/src/lib.rs
+++ exam/reverse_it/src/lib.rs
@@ -1,27 +1,10 @@
-// pub fn reverse_it(v: i32) -> String {
-//     let z = if v < 0 { "-" } else { "" };
-//     let binding = v.to_string();
-//     let num = binding.trim_matches(|c| c == '-');
-//     let reverse = num.to_string().chars().rev().collect::<String>();
-//     format!("{}{}{}", z, reverse, num)
-// }
-
-//=====================================
-
 pub fn reverse_it(v: i32) -> String {
-    let mut x = v;
-    let mut y :i32 = 0;
-    let mut z = 1;
+    let x = v.abs().to_string(); // نحصل على القيمة المطلقة كـ String
+    let reversed: String = x.chars().rev().collect(); // نعمل لها reverse
+
     if v < 0 {
-        z = -1;
-        x *= z;
+        format!("-{}{}", reversed, x) // إذا الرقم سلبي، نضيف -
+    } else {
+        format!("{}{}", reversed, x) // إذا الرقم موجب
     }
-    while x != 0 {
-        y *= 10;
-        y += x % 10;
-        x /= 10;
-        //println!("y = {y}, x = {x}");
-    }
-    y *= z;
-    y.to_string()
 }
