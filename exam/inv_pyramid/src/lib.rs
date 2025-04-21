71e968351460922a548642df5658159afae0e069
unknown <jehadalami11@gmail.com>
2025-04-21 16:04:39 +0300

    rust

--- exam/inv_pyramid/src/lib.rs
+++ exam/inv_pyramid/src/lib.rs
@@ -1,39 +1,10 @@
-fn inv_pyramid(v: String, i: u32) -> Vec<String> {
-    let mut result = Vec::new();
-    
-    // الجزء العلوي من الهرم (التوسع)
-    for j in 1..=i {
-        let spaces = " ".repeat((i - j) as usize);
-        let symbols = v.repeat(j as usize);
-        result.push(format!("{}{}", spaces, symbols));
+pub fn inv_pyramid(v: String, i: u32) -> Vec<String> {
+    let mut pyramid = Vec::new();
+    for j in 1..i{
+        pyramid.push(format!("{}{}", " ".repeat(j as usize), v.repeat(j as usize)));
     }
-
-    // الجزء السفلي من الهرم (التقليص)
-    for j in (1..i).rev() {
-        let spaces = " ".repeat((i - j) as usize);
-        let symbols = v.repeat(j as usize);
-        result.push(format!("{}{}", spaces, symbols));
+    for j in (1..=i).rev() {
+        pyramid.push(format!("{}{}", " ".repeat(j as usize), v.repeat(j as usize)));
     }
-
-    result
-}
-
-fn main() {
-    let a = inv_pyramid(String::from("#"), 1);
-    let b = inv_pyramid(String::from("a"), 2);
-    let c = inv_pyramid(String::from(">"), 5);
-    let d = inv_pyramid(String::from("&"), 8);
-
-    for v in a.iter() {
-        println!("{:?}", v);
-    }
-    for v in b.iter() {
-        println!("{:?}", v);
-    }
-    for v in c.iter() {
-        println!("{:?}", v);
-    }
-    for v in d.iter() {
-        println!("{:?}", v);
-    }
+    pyramid
 }
