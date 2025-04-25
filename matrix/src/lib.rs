8b98030fa4d84b650cecd68493cf0c928b634b50
unknown <jehadalami11@gmail.com>
2025-04-25 21:32:11 +0300

    rust

--- matrix/src/lib.rs
+++ matrix/src/lib.rs
@@ -1,3 +1,6 @@
+use std::ops::Mul;
+use std::fmt::Debug;
+
 // Define the Scalar trait
 pub trait Scalar: Clone {
     fn zero() -> Self;
@@ -21,10 +24,10 @@ impl Scalar for f64 {
 }
 
 // Matrix struct
-#[derive(Debug, PartialEq, Eq)]
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Matrix<T>(pub Vec<Vec<T>>);
 
-// Matrix impl with new, zero, identity
+// Implement basic constructors
 impl<T: Scalar> Matrix<T> {
     pub fn new() -> Matrix<T> {
         Matrix(vec![vec![T::zero()]])
@@ -40,5 +43,58 @@ impl<T: Scalar> Matrix<T> {
             mat[i][i] = T::one();
         }
         Matrix(mat)
+    }
+}
+
+// Matrix utility methods
+impl<T: Clone> Matrix<T> {
+    pub fn number_of_cols(&self) -> usize {
+        if self.0.is_empty() || self.0[0].is_empty() {
+            0
+        } else {
+            self.0[0].len()
+        }
+    }
+
+    pub fn number_of_rows(&self) -> usize {
+        self.0.len()
+    }
+
+    pub fn row(&self, n: usize) -> Vec<T> {
+        self.0[n].clone()
+    }
+
+    pub fn col(&self, n: usize) -> Vec<T> {
+        self.0.iter().map(|row| row[n].clone()).collect()
+    }
+}
+
+// Matrix multiplication
+impl<T> Mul for Matrix<T>
+where
+    T: Copy + Mul<Output = T> + std::iter::Sum + Default + Debug,
+{
+    type Output = Option<Matrix<T>>;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        if self.number_of_cols() != rhs.number_of_rows() {
+            return None;
+        }
+
+        let rows = self.number_of_rows();
+        let cols = rhs.number_of_cols();
+        let common = self.number_of_cols();
+
+        let mut result = vec![vec![T::default(); cols]; rows];
+
+        for i in 0..rows {
+            for j in 0..cols {
+                result[i][j] = (0..common)
+                    .map(|k| self.0[i][k] * rhs.0[k][j])
+                    .sum();
+            }
+        }
+
+        Some(Matrix(result))
     }
 }
--- matrix/src/main.rs
+++ matrix/src/main.rs
@@ -1,8 +1,14 @@
 use matrix::*;
 
 fn main() {
-	let m: Matrix<u32> = Matrix(vec![vec![0, 0, 0, 0], vec![0, 0, 0, 0], vec![0, 0, 0, 0]]);
-	println!("{:?}", m);
-	println!("{:?}", Matrix::<i32>::identity(4));
-	println!("{:?}", Matrix::<f64>::zero(3, 4));
+    let matrix: Matrix<u32> = Matrix(vec![vec![3, 6], vec![8, 0]]);
+    println!("{:?}", matrix.col(0));
+    println!("{:?}", matrix.row(1));
+
+    let matrix_1: Matrix<u32> = Matrix(vec![vec![0, 1], vec![0, 0]]);
+    let matrix_2: Matrix<u32> = Matrix(vec![vec![0, 0], vec![1, 0]]);
+    let mult = matrix_1.clone() * matrix_2.clone();
+    println!("{:?}", mult);
+    println!("{:?}", matrix_1.number_of_cols());
+    println!("{:?}", matrix_2.number_of_rows());
 }
