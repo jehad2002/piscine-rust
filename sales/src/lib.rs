a53a18961016f9b82017ab79eb299fb1976da8bd
unknown <jehadalami11@gmail.com>
2025-04-27 16:29:40 +0300

    rust

--- sales/src/lib.rs
+++ sales/src/lib.rs
@@ -1,18 +1,15 @@
 // sales/src/lib.rs
 
-// تعريف هيكل Store الذي يحتوي على قائمة من المنتجات مع أسعارها.
 #[derive(Debug)]
 pub struct Store {
     products: Vec<(String, f32)>,
 }
 
 impl Store {
-    // دالة جديدة لإنشاء Store مع قائمة من المنتجات.
     pub fn new(products: Vec<(String, f32)>) -> Store {
         Store { products }
     }
 
-    // دالة لإيجاد سعر المنتج باستخدام اسمه.
     pub fn get_price(&self, product_name: &str) -> Option<f32> {
         self.products.iter().find_map(|(name, price)| {
             if name == product_name {
@@ -24,19 +21,16 @@ impl Store {
     }
 }
 
-// تعريف هيكل Cart الذي يحتوي على قائمة المنتجات التي تم إضافتها.
 #[derive(Debug)]
 pub struct Cart {
     items: Vec<String>,
 }
 
 impl Cart {
-    // دالة جديدة لإنشاء Cart فارغ.
     pub fn new() -> Cart {
         Cart { items: Vec::new() }
     }
 
-    // دالة لإضافة منتج إلى العربة.
     pub fn insert_item(&mut self, store: &Store, product_name: String) {
         if store.get_price(&product_name).is_some() {
             self.items.push(product_name);
--- sales/src/main.rs
+++ sales/src/main.rs
@@ -3,20 +3,15 @@
 use sales::*;  // استيراد مكتبة sales
 
 fn main() {
-    // إنشاء متجر يحتوي على قائمة من المنتجات وأسعارها.
     let store = Store::new(vec![
         (String::from("product A"), 1.23),
         (String::from("product B"), 23.1),
         (String::from("product C"), 3.12),
     ]);
 
-    // طباعة المعلومات الخاصة بالمتجر.
     println!("{:?}", store);
 
-    // إنشاء عربة تسوق فارغة.
     let mut cart = Cart::new();
-    
-    // إضافة منتجات إلى العربة.
     cart.insert_item(&store, String::from("product A"));
     cart.insert_item(&store, String::from("product B"));
     cart.insert_item(&store, String::from("product C"));
@@ -24,6 +19,6 @@ fn main() {
     // طباعة الإيصال مع تمرير المتجر كوسيلة
     println!("{:?}", cart.generate_receipt(&store));
 
-    // طباعة العربة نفسها.
+    // طباعة العربة نفسها
     println!("{:?}", cart);
 }
