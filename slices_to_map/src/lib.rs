94eb0cc31dfe007c8e052f744ba2a6ee60a8cee8
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2025-04-28 14:44:48 +0300

    rust

--- slices_to_map/src/lib.rs
+++ slices_to_map/src/lib.rs
@@ -1,5 +1,5 @@
 use std::collections::HashMap;
-use std:#️⃣:Hash;
+use std::hash::Hash;
 
 pub fn slices_to_map<'a, T: Eq + Hash, U>(first_slice: &'a [T], second_slice: &'a [U]) -> HashMap<&'a T, &'a U> {
     let mut length: usize = 0;
@@ -8,7 +8,6 @@ pub fn slices_to_map<'a, T: Eq + Hash, U>(first_slice: &'a [T], second_slice: &'
     
     let mut result = HashMap::new();
 
-    
     if length_first == length_second{
         length = length_first;
     } else if length_first > length_second {
@@ -17,7 +16,6 @@ pub fn slices_to_map<'a, T: Eq + Hash, U>(first_slice: &'a [T], second_slice: &'
         length = length_first;
     }
 
-    
     for ind in 0..length{
         result.insert(&first_slice[ind], &second_slice[ind]);
     }
@@ -31,6 +29,11 @@ mod tests {
 
     #[test]
     fn it_works() {
-        
+        let first = ["a", "b", "c"];
+        let second = [1, 2, 3];
+        let result = slices_to_map(&first, &second);
+        assert_eq!(result.get(&"a"), Some(&1));
+        assert_eq!(result.get(&"b"), Some(&2));
+        assert_eq!(result.get(&"c"), Some(&3));
     }
 }
