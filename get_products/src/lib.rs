3273270b5d8a6a0149e79b00fe30383a03510543
adam-06-05 <adam-06-05@adam-06-05s-Mac-mini.local>
2025-04-28 13:32:58 +0300

    rust

---
+++ get_products/src/lib.rs
@@ -0,0 +1,30 @@
+pub fn get_products(arr: Vec<usize>) -> Vec<usize> {
+    let mut result: Vec<usize> = Vec::new();
+
+    if arr.len() == 1{
+        return result;
+    }
+
+    for cur_ind in 0..arr.len() {
+        let mut product = 1;
+        for ind in 0..arr.len() {
+            if ind != cur_ind{
+                product *= arr[ind];
+            }
+        }
+        result.push(product);
+    }
+
+    return result;
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        
+    }
+}
